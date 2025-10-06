8f7b7ceb0fda65386c35d5b15c4ac21354577ddd
a-krawczynski <a.krawczynski@wustl.edu>
2025-10-06 15:36:01 -0500

    heart

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,27 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		// base !!!
+		StdDraw.setPenColor(178,255,255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+		StdDraw.setPenColor(251,172,190);
+		StdDraw.filledRectangle(0.5, 0.75, 0.5, 0.25);
+
+		StdDraw.setPenColor(44,73,127);
+		
+		// sword !!!
+		StdDraw.filledRectangle(0.5, 0.5, 0.05, 0.4);
+
+		StdDraw.setPenColor(Color.white);
+
+		// heart !!!
+		// diamonds
+		double[] xd = {.5, 0.75, 0.5, 0.25};
+    	double[] yd = {0.25, 0.5, 0.65, 0.5};
+    	StdDraw.filledPolygon(xd, yd);
+	
+    	// half circles
+    	StdDraw.filledCircle(0.65, 0.61, 0.15);
+    	StdDraw.filledCircle(0.35, 0.61, 0.15);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,23 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int blue = in.nextInt();
+		int green = in.nextInt();
+		boolean fill = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(red, blue, green); 
+		if (fill == true) {
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} else {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		in.close();
 	}
 }
